fe240934f837a77e4ea243d0e7915ebd40647b4b
Kcisti <91692567+Kcisti@users.noreply.github.com>
2022-02-05 23:56:22 +0100

    69347867094984758979'84869

---
+++ script.js
@@ -0,0 +1,112 @@
+document.getElementById('s').style.display='none';
+function connect(connect){
+  document.getElementById('w').style.display='none';
+  document.getElementById('s').style.display='block';
+  var membrana = document.getElementById('signin_input').value;
+  console.log(membrana);
+
+  const CLIENT_ID = 'EFslC1FFGukiWwqQ';
+  const drone = new ScaleDrone(CLIENT_ID, {
+    data: { name: membrana}
+  });
+
+  let members = [];
+
+  drone.on('open', error => {
+    if (error) {
+      return console.error(error);
+    }
+    console.log('Successfully connected to Scaledrone');
+
+    const room = drone.subscribe('observable-room');
+    room.on('open', error => {
+      if (error) {
+        return console.error(error);
+      }
+      console.log('Successfully joined room');
+    });
+
+    room.on('members', m => {
+      members = m;
+      updateMembersDOM();
+    });
+
+    room.on('member_join', member => {
+      members.push(member);
+      updateMembersDOM();
+    });
+
+    room.on('member_leave', ({id}) => {
+      const index = members.findIndex(member => member.id === id);
+      members.splice(index, 1);
+      updateMembersDOM();
+    });
+
+    room.on('data', (text, member) => {
+      if (member) {
+        addMessageToListDOM(text, member);
+      } else {
+        // Message is from server
+      }
+    });
+  });
+
+  const DOM = {
+    membersCount: document.querySelector('.members-count'),
+    messages: document.querySelector('.messages'),
+    input: document.querySelector('.message-form__input'),
+    form: document.querySelector('.message-form'),
+  };
+
+  DOM.form.addEventListener('submit', sendMessage);
+
+  function sendMessage() {
+    const value = DOM.input.value;
+    if (value === '') {
+      return;
+    }
+    DOM.input.value = '';
+    drone.publish({
+      room: 'observable-room',
+      message: value,
+    });
+  }
+
+  function createMessageElement(text, member) {
+    const el = document.createElement('div');
+    el.appendChild(createMemberElement(member));
+    el.appendChild(document.createTextNode(text));
+    el.className = 'message';
+    el.setAttribute('id','mess');
+    return el;
+  }
+
+  function addMessageToListDOM(text, member) {
+    const el = DOM.messages;
+    const wasTop = el.scrollTop === el.scrollHeight - el.clientHeight;
+    el.appendChild(createMessageElement(text, member));
+    if (wasTop) {
+      el.scrollTop = el.scrollHeight - el.clientHeight;
+    }
+  }
+
+  function createMemberElement(member) {
+    const {name} = member.clientData;
+    const el = document.createElement('div');
+    el.appendChild(document.createTextNode(name));
+    el.className = 'member';
+    return el;
+  }
+
+  function updateMembersDOM() {
+    DOM.membersCount.innerText = `${members.length} users in room`;
+    navigator.serviceWorker.register('sw.js');
+    Notification.requestPermission(function(result) {
+      if (result === 'granted') {
+        navigator.serviceWorker.ready.then(function(registration) {
+          registration.showNotification('ROOM',
+          {body :`${members.length} users in room`})});
+        }
+      });
+  }
+  }
